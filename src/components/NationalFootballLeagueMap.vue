679ce9ec828ae2edf327b4c1ae81d899b0edb64f
ville <ville@example.com>
2020-12-22 21:32:12 +0200

    integrated vue2-leaflet, created route for NFL map and related component

---
+++ src/components/NationalFootballLeagueMap.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="height: 350px">
+    <l-map style="height: 80%; width: 100%" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      zoom: 3,
+      center: [47.41322, -1.219482],
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ src/views/NFL.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This page will host a map with NFL stadium locations</h1>
+    <national-football-league-map />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NationalFootballLeagueMap from '@/components/NationalFootballLeagueMap.vue'
+
+export default {
+  name: 'NFL',
+  components: {
+    NationalFootballLeagueMap
+  }
+}
+</script>
