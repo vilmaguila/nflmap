af196716bdf6ab7b0070ca8a3f812c6696ee13dd
ville <ville@example.com>
2021-01-06 19:47:59 +0200

    [WIP] created hardcoded filtering for layers

---
+++ src/common/init_nfl.js
@@ -0,0 +1,6 @@
+const init_data = [
+    { conference: 'NFC', visible: true },
+    { conference: 'AFC', visible: true }
+]
+
+export default init_data;
--- src/components/NationalFootballLeagueMap.vue
+++ src/components/NationalFootballLeagueMap.vue
@@ -2,36 +2,51 @@
   <div style="height: 350px">
     <l-map style="height: 80%; width: 100%" :zoom="zoom" :center="center">
       <l-tile-layer :url="url"></l-tile-layer>
-      <l-geo-json :geojson="data" :options="options"/>
+      <!-- <l-geo-json :geojson="data" :options="options"/> -->
+      <l-geo-json
+        v-for="(item, index) in data_conf"
+        :key="index"
+        :geojson="data"
+        :visible="item.visible"
+        :options="options"
+      />
     </l-map>
   </div>
 </template>
 
 <script>
 import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
+import init_data from "../common/init_nfl.js";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LGeoJson
+    LGeoJson,
   },
   props: {
-      data: Object
+    data: Object,
   },
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       zoom: 3,
       center: [47.41322, -1.219482],
+      data_conf: init_data,
     };
   },
   methods: {},
   computed: {
     options() {
       return {
-        onEachFeature: this.onEachFeatureFunction
+        onEachFeature: this.onEachFeatureFunction,
+        filter: this.filterFunction,
       };
+    },
+    filterFunction() {
+      return (feature) => {
+        return feature.properties.Conference === 'AFC'
+      }
     },
     onEachFeatureFunction() {
       return (feature, layer) => {
@@ -44,7 +59,7 @@ export default {
           { permanent: false, sticky: true }
         );
       };
-    }
-  }
+    },
+  },
 };
 </script>
